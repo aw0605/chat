b0c39c46bac8d98f5fd27df97b816078b3eddca5
aw0605 <101087917+aw0605@users.noreply.github.com>
2023-07-03 02:25:07 +0900

    chat 수정 및 삭제 구현
    chat 내용을 수정하는 버튼과 삭제하는 버튼을 각각 put과 delete를 이용해 구현하였다.

--- main.py
+++ main.py
@@ -19,4 +19,20 @@ def create_chat(chat: Chat):
 def read_chat():
     return chats
 
+@app.put("/chats/{chat_id}")
+def put_chat(req_chat: Chat):
+    for chat in chats:
+        if chat.id == req_chat.id:
+            chat.content = req_chat.content
+            return '내용을 성공적으로 수정했습니다.'
+    return '해당 메모는 존재하지 않습니다. 수정에 실패했습니다.'
+
+@app.delete("/chats/{chat_id}")
+def delete_chat(chat_id):
+    for index, chat in enumerate(chats):
+        if chat.id == chat_id:
+            chats.pop(index)
+            return "내용 삭제에 성공했습니다."
+    return "해당 메모는 존재하지 않습니다. 삭제에 실패했습니다."
+
 app.mount("/", StaticFiles(directory='static', html=True), name='static')
--- static/app.js
+++ static/app.js
@@ -1,9 +1,48 @@
+async function editChat(e) {
+  const id = e.target.dataset.id;
+  const editInput = prompt("수정할 내용을 입력해주세요");
+
+  const res = await fetch(`/chats/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      content: editInput,
+    }),
+  });
+  readChat();
+}
+
+async function deleteChat(e) {
+  const id = e.target.dataset.id;
+
+  const res = await fetch(`/chats/${id}`, {
+    method: "DELETE",
+  });
+  readChat();
+}
+
 function showChat(chat) {
   const ul = document.querySelector(".showChat");
   const li = document.createElement("li");
-  li.innerText = chat.content;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정";
+  editBtn.dataset.id = chat.id;
+  editBtn.addEventListener("click", editChat);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "삭제";
+  deleteBtn.dataset.id = chat.id;
+  deleteBtn.addEventListener("click", deleteChat);
+
+  li.innerText = `${chat.content}`;
 
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
 }
 
 async function readChat() {
